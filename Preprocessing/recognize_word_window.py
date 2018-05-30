7f85b8994c14af92dd507e7c24a0116545433700
paulozk <p.m.ozkohen@student.rug.nl>
2018-05-30 14:29:08 +0100

    Sentences/words are now written to a txt file

--- Preprocessing/recognize_word_window.py
+++ Preprocessing/recognize_word_window.py
@@ -3,6 +3,59 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from recognize_character_cnn import *
+import itertools
+import pandas as pd
+
+
+# for each ngram, check if they occur as ngrams for words in the ngram file
+def find_Ngram(gram, current_n, names, frequencies):
+    count = 0
+    for word in names:
+        #take the percentage of the ngram out of the entire word
+        proportion = current_n / len(word.split('_'))
+        # compare gram with all current_n grams of each word
+        if(gram in word):
+            index = names.index(word)
+            count += proportion * frequencies[index]
+    #print(gram, 'weighted frequency:',  count)
+    return count
+
+
+
+# compute the frequency of the word by summing the frequency of all of its ngrams
+def compute_frequency(word_string, n_chars, names, frequencies):
+    # a word with N characters has N-1 bigrams
+ 
+    N_grams = n_chars - 1
+    current_n = 2
+    total_sum = 0
+    while(current_n <= n_chars):
+        start = 0
+        end = current_n
+        # for each ngram, check if they occur as ngrams for words in the ngram file
+        for i in range(N_grams):                  
+            gram = "_".join(word_string[start:end])
+            gram = gram.lower()
+            total_sum += find_Ngram(gram, current_n, names, frequencies)  
+            """try:
+                 # find the index of the recognized word in the ngrams file
+                index = names.index(gram)
+                # find the frequency of the word
+                freq = freqs[index]
+                print("N gram:", N_grams, "index:", i)
+                total_sum += freq
+            except:
+                pass
+            """
+            start += 1
+            end += 1
+          
+        current_n += 1
+        N_grams -= 1
+        
+    return total_sum
+
+
 
 # recognize the characters in a word by sliding a window across big
 # connected components and recognizing characters within
@@ -32,12 +85,19 @@ def recognize_word(word, avg_width):
     n_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(word, 8, cv2.CV_32S)
     # remove background component
     stats = stats[1:]
+    
+    stats = stats[stats[:,0].argsort()]
     components = [] 
     # plot all components
     for stat in stats:
+        
         if(stat[2] > (0.5 * avg_width) and stat[3] > (0.5 * avg_width) and stat[2] < (avg_width * 5) and stat[3] < (avg_width * 5)):
+            print(stat[0])
             components.append(stat)
-    
+          
+    candidates = [] 
+    n_characters = 0
+    components = list(np.flip(np.array(components), 0))
     # for every component:
     for component in components:
        
@@ -103,6 +163,7 @@ def recognize_word(word, avg_width):
                         plt.show()
                         
                         top_chars, top_scores = recognize_character(window)
+                        candidates.append(top_chars)
                         
                         print("Recognition result:", top_chars, top_scores)
                         print("---------------------------------------------------\n")
@@ -133,9 +194,39 @@ def recognize_word(word, avg_width):
             plt.imshow(component_image, cmap='gray', aspect = 1)
             plt.show()
             top_chars, top_scores = recognize_character(component_image)
+            candidates.append(top_chars)
             print("Recognition result:", top_chars, top_scores)
             print("---------------------------------------------------\n")
         print()
+    print("Candidates:", candidates)
+    
+    combinations = list(itertools.product(*candidates))
+    print("Combinations:", combinations)
+    
+    # find the most frequent combination
+    ngrams = pd.read_excel('n_grams_probs.xlsx')
+    names = list(ngrams['Names'])
+    freqs = list(ngrams['Frequencies'])
+    names = list(map(lambda x: x.lower(), names))
+    
+    best_freq_index = 0
+    best_freq = 0
+    
+    for combi in combinations:    
+        freq = compute_frequency("_".join(combi), len(candidates), names, freqs)
+        if(freq > best_freq):
+            best_freq = freq
+            best_freq_index = combinations.index(combi)
+        print("Combi:", combi, "Frequency:", freq)
+    
+    best = "_".join(list(combinations[best_freq_index]))
+    print("Best word:", best)
+    return best
+        
+  
+        
+        
+        
 
     
     
